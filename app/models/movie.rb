832c366d773afd407ae6dc012c5ee0bd2ed79302
yojisekiya <yojisekiya@gmail.com>
2022-06-06 23:04:10 +0900

    「タスク4」で作成した2種類のモデルの各カラムに空データが入らないようにするバリデーションを追加

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  with_options presense: true do
+    validates :genre
+    validates :title
+    validates :url
+  end
 end
--- app/models/text.rb
+++ app/models/text.rb
@@ -1,2 +1,7 @@
 class Text < ApplicationRecord
+  with_options presense: true do
+    validates :genre
+    validates :title
+    validates :content
+  end
 end
