12d593bb91ff5b83908bd79b24a0c394b52a24a2
Aratahead <maramhisa.1229@gmail.com>
2022-06-10 13:12:31 +0900

    テキスト教材と動画教材のCSVのデータを投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@ User.find_or_create_by!(email: email) do |user|
   user.password = password
   puts "ユーザーの初期データインポートに成功しました。"
 end
+
+# texts, movies テーブルを再生成（関連付くテーブルを含む）
+%w[texts movies].each do |table_name|
+  ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table_name} RESTART IDENTITY CASCADE")
+end
+
+require "csv"
+
+CSV.foreach("db/csv_data/text_data.csv", headers: true) do |row|
+  Text.create!(row.to_h)
+end
+
+CSV.foreach("db/csv_data/movie_data.csv", headers: true) do |row|
+  Movie.create!(row.to_h)
+end
