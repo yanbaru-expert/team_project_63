8e11821ed6daac51e539f337a7ef29e408f07d9a
yojisekiya <yojisekiya@gmail.com>
2022-06-25 01:50:09 +0900

    db/seeds.rb に追加された AdminUser.create! の1行を User と同様の形式に修正（何度実行してもエラーが出ないようにする）

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@ User.find_or_create_by!(email: email) do |user|
   puts "ユーザーの初期データインポートに成功しました。"
 end
 
+admin_email = "admin@example.com"
+
+AdminUser.find_or_create_by!(email: admin_email) do |user|
+  user.password = password
+  puts "管理者の初期データインポートに成功しました。"
+end
+
 # texts, movies テーブルを再生成（関連付くテーブルを含む）
 %w[texts movies].each do |table_name|
   ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table_name} RESTART IDENTITY CASCADE")
@@ -20,4 +27,3 @@ end
 CSV.foreach("db/csv_data/movie_data.csv", headers: true) do |row|
   Movie.create!(row.to_h)
 end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
