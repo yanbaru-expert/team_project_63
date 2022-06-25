e6bb7ad03bc0f2e6bbe0079dada7b03b99a1ecaf
yojisekiya <yojisekiya@gmail.com>
2022-06-25 12:14:17 +0900

    管理画面の各ページのカラム・フォームを修正

--- app/admin/texts.rb
+++ app/admin/texts.rb
@@ -13,4 +13,27 @@ ActiveAdmin.register Text do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
+  index do
+    selectable_column
+    id_column
+    column :genre
+    column :title
+    actions
+  end
+
+  filter :genre, as: :select, collection: Movie.genres_i18n.invert.transform_values { |v| Movie.genres[v] }
+  filter :title
+  filter :content
+  filter :created_at
+  filter :updated_at
+
+  form do |f|
+    f.inputs do
+      f.input :genre, as: :select, collection: Movie.genres_i18n.invert
+      f.input :title
+      f.input :content
+    end
+    f.actions
+  end
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -13,4 +13,24 @@ ActiveAdmin.register User do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :created_at
+    actions
+  end
+
+  filter :email
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
 end
