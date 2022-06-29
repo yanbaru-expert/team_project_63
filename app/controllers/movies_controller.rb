e0118f6f906e5a9e50bf108a0513d4286bda8b3b
yojisekiya <yojisekiya@gmail.com>
2022-06-29 22:59:18 +0900

    PHP動画教材ページで「PHPの動画教材のみ」が表示されるように修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.where(genre: Movie::RAILS_GENRE_LIST)
+    @movies = if params[:genre] == "php"
+                Movie.where(genre: Movie::PHP_GENRE_LIST)
+              else
+                Movie.where(genre: Movie::RAILS_GENRE_LIST)
+              end
   end
 end
