38446ff55370979b9ff0bac4331e62749bd6152f
Aratahead <maramhisa.1229@gmail.com>
2022-09-06 08:30:00 +0900

    非同期で読破済みを切り替える

--- app/controllers/watch_progresses_controller.rb
+++ app/controllers/watch_progresses_controller.rb
@@ -1,11 +1,11 @@
 class WatchProgressesController < ApplicationController
   def create
-    current_user.watch_progresses.create!(movie_id: params[:movie_id])
-    redirect_back(fallback_location: root_path)
+    @movie = Movie.find(params[:movie_id])
+    current_user.watch_progresses.create!(movie_id: @movie.id)
   end
 
   def destroy
-    current_user.watch_progresses.find_by(movie_id: params[:movie_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    @movie = Movie.find(params[:movie_id])
+    current_user.watch_progresses.find_by(movie_id: @movie.id).destroy!
   end
 end
